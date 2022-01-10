3c6c1f71dbb007d99d66fabb43954c8d2b6bd34b
Eamon Conway <ejconway.research@gmail.com>
2022-01-10 12:30:48 +1100

    Including header guard for abm_random.h
    Former-commit-id: ef98c86b79ebaebcedec9728800252a62d97e502

--- include/abm/abmrandom.h
+++ include/abm/abmrandom.h
@@ -1,3 +1,10 @@
+#ifndef WANEWORLD_ABM_ABMRANDOM_H 
+#define WANEWORLD_ABM_ABMRANDOM_H
 #include<random> 
+/**
+ * @brief Define random number generation here. This allows for an easy location to alter the seed etc.
+ * 
+ */
 extern std::default_random_engine generator;
 extern std::uniform_real_distribution<double> genunf_std;
+#endif
