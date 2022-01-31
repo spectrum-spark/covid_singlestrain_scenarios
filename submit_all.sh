7eebcf84a4025f7394636e6055950c6abc10114b
Eamon Conway <ejconway.research@gmail.com>
2022-01-31 19:54:31 +1100

    Updated
    Former-commit-id: d3402152e1cb4fbbaf3aeff56b953b32e3b231de

--- submit_all.sh
+++ submit_all.sh
@@ -5,6 +5,6 @@ do
 for LEVEL in baseline low high
 do
   echo "${STATE}_${LEVEL}"
-  sbatch submit_function.script --job-name=${STATE}_${LEVEL} --export=STATE_INPUT=${STATE},LEVEL_INPUT=${LEVEL},
+  sbatch --job-name=${STATE}_${LEVEL} --export=STATE_INPUT=${STATE},LEVEL_INPUT=${LEVEL} submit_function.script 
 done
 done
