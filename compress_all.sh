6e8fa9147e2d3b56f7434c95f492bf214ee968f0
Eamon Conway (Monash University) <eamonc@m3-login2.massive.org.au>
2022-02-13 17:39:25 +1100

    Submit all updated to also run compression afterwards
    Former-commit-id: 8825c37212b57b0be6a84ad54adba5ad7db89a20

--- compress_all.sh
+++ compress_all.sh
@@ -14,18 +14,26 @@
 # for SCENARIO in jan28 bp15_jan28 bn15_jan28 
 # do
 
-for STATE in NSW
-do 
-for ASC in 33 50
-do
-for RED in 50 
-do 
-for LEVEL in baseline low moderate
-do
+# for STATE in NSW
+# do 
+# for ASC in 33 50
+# do
+# for RED in 50 
+# do 
+# for LEVEL in baseline low moderate
+# do
 # for STATE in NSW
 # do 
 # for SCENARIO in high_jan28_50final low_jan28_50final baseline_jan28_50final
 # do
+for STATE in TAS
+do 
+for ASC in 33 
+do
+for RED in 50 
+do 
+for LEVEL in low  
+do
   # echo "${STATE}_${SCENARIO}"
   echo "${STATE}_${ASC}A_${RED}R_${LEVEL}"
   sbatch --job-name=${STATE}_${SCENARIO} --export=STATE_INPUT=${STATE},SCENARIO_INPUT=${ASC}A_${RED}R_${LEVEL} compress_function.script 
@@ -33,4 +41,3 @@ done
 done
 done
 done
-
