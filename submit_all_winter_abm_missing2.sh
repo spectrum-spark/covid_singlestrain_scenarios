d249e7018cec747481f27de532925409877693ed
Thao Phuong Le <tk.le@unimelb.edu.au>
2022-05-05 15:56:19 +1000

    submit file changes
    Former-commit-id: 636948a57af5906abca0a02c0ef7431d63602d96

--- submit_all_winter_abm_missing2.sh
+++ submit_all_winter_abm_missing2.sh
@@ -2,9 +2,8 @@
 
 for POPTYPE in younger
 do
-for (( PARAMFILENUM=4; PARAMFILENUM<=4; PARAMFILENUM++ ))
+for PARAMFILENUM in 1 13
 do
 sbatch --parsable --array=1-25 --job-name=winter --export=POPULATION_INPUT=${POPTYPE},PARAM_FILE=${PARAMFILENUM} submit_function_winter.script
-sleep 1m
 done
 done
